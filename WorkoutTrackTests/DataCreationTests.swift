e9ced14d2c8fae87e4945b29d24353d5131c13e1
myh <huang.minya@northeastern.edu>
2024-02-01 00:48:55 -0500

    Add DataCreationTests

---
+++ WorkoutTrackTests/DataCreationTests.swift
@@ -0,0 +1,12 @@
+//
+//  DataCreationTests.swift
+//  GYMHackTests
+//
+//  Created by Min-Yang Huang on 2024/2/1.
+//
+
+import XCTest
+
+final class DataCreationTests: XCTestCase {
+    func test() {}
+}
