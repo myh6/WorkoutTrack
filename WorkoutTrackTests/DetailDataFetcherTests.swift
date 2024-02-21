aa67ab4a023572e38ff27084a16261f150899a9d
myh <huang.minya@northeastern.edu>
2024-02-21 14:12:57 -0500

    DetailDataFetcher does not message store upon creation

--- WorkoutTrackTests/DetailDataFetcherTests.swift
+++ WorkoutTrackTests/DetailDataFetcherTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import GYMHack
 
 class DetailDataFetcher {
     
@@ -15,7 +16,34 @@ final class DetailDataFetcherTests: XCTestCase {
 
     func test_init_doesNotMessageStore() {
         let sut = DetailDataFetcher()
-        let store = 
+        let store = DetailFeedStoreSpy()
+        
+        XCTAssertTrue(store.receivedMessage.isEmpty)
+    }
+    
+    //MARK: - Helper
+    
+    class DetailFeedStoreSpy: DetailAdditionStore {
+        private var addDetailCompletion = [AddDataCompletion]()
+        
+        func addData(details: [DetailedDTO], completion: @escaping (Error?) -> Void) {
+            receivedMessage.append(.addData(details))
+            addDetailCompletion.append(completion)
+        }
+        
+        func completeAddDetailSuccessfully(at index: Int = 0) {
+            addDetailCompletion[index](nil)
+        }
+        
+        func completeAddDetail(with error: NSError, at index: Int = 0) {
+            addDetailCompletion[index](error)
+        }
+        
+        enum ReceiveMessage: Equatable {
+            case addData([DetailedDTO])
+        }
+        
+        var receivedMessage = [ReceiveMessage]()
     }
 
 }
