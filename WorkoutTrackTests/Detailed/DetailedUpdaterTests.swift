1c83023c718129f687f35d969b2785f6889986d5
myh <huang.minya@northeastern.edu>
2024-02-25 18:16:25 -0500

    Extract the creation of store and sut into a factory helper method

--- WorkoutTrackTests/Detailed/DetailedUpdaterTests.swift
+++ WorkoutTrackTests/Detailed/DetailedUpdaterTests.swift
@@ -22,19 +22,26 @@ class DetailedDataUpdater {
 final class DetailedUpdaterTests: XCTestCase {
 
     func test_init_doesNotMessageStore() {
-        let store = DetailedDTOStoreSpy()
-        let sut = DetailedDataUpdater(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertTrue(store.receivedMessage.isEmpty)
     }
     
     func test_updateDetailed_requestDataUpdateWithID() {
-        let store = DetailedDTOStoreSpy()
-        let sut = DetailedDataUpdater(store: store)
+        let (sut, store) = makeSUT()
         let anyID = UUID().uuidString
         
         sut.updateDetailed(with: anyID)
         
         XCTAssertEqual(store.receivedMessage, [.update(anyID)])
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: DetailedDataUpdater, store: DetailedDTOStoreSpy) {
+        let store = DetailedDTOStoreSpy()
+        let sut = DetailedDataUpdater(store: store)
+        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(store)
+        return (sut, store)
     }
 }
