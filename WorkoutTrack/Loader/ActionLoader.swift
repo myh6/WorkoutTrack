f5e0fe446f9782656bf2e4b21b82f0222b689711
myh <huang.minya@northeastern.edu>
2024-02-25 16:43:13 -0500

    Make ActionDataLoader conform to ActionLoader protocol to preapre for dependency injection

--- WorkoutTrack/Loader/ActionLoader.swift
+++ WorkoutTrack/Loader/ActionLoader.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+public protocol ActionLoader {
+    func loadAction(with predicate: NSPredicate?, completion: @escaping (ActionRetrievalResult) -> Void)
+}
+
+public extension ActionLoader {
+    func loadAction(completion: @escaping (ActionRetrievalResult) -> Void) {
+        loadAction(with: nil, completion: completion)
+    }
+}
+
 public enum ActionRetrievalResult {
     public struct ActionFeed: Equatable {
         let actionName: String
