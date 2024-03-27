36236274bfaa394256f4ac7ef6b4e4611958fadc
myh <huang.minya@northeastern.edu>
2024-03-27 00:54:53 -0400

    Make sure that operation with side effects run serially

--- WorkoutTrackTests/Action/CoreDataActionStoreTests.swift
+++ WorkoutTrackTests/Action/CoreDataActionStoreTests.swift
@@ -121,6 +121,35 @@ final class CoreDataActionStoreTests: XCTestCase {
         
         expect(sut, with: nil, toRetrieve: .success([]))
     }
+    
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        
+        var completedOperationInOrder = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.addAction(action: [anyAction().local]) { _ in
+            completedOperationInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Opeartion 2")
+        sut.retrieve(predicate: nil) { _ in
+            completedOperationInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.remove(actionID: UUID()) { _ in
+            completedOperationInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order: \(completedOperationInOrder)")
+    }
+    
     //MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataActionStore {
         let storeBundle = Bundle(for: CoreDataActionStore.self)
